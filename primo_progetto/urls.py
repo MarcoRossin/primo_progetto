ed044ac27db2f4511b84f10d598522e5a74f91d1
MarcoRossin <marco.rossin04@gmail.com>
2022-10-10 10:08:51 +0200

    aggiornamento

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('prima_app/',include("prima_app.urls",namespace="prima_app"))
+    path('prima_app/',include("prima_app.urls",namespace="prima_app")),
 ]

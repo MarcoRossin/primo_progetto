12bd5eccc83010996f61f5f9cda082c6b3be2891
MarcoRossin <marco.rossin04@gmail.com>
2022-12-05 08:59:53 +0100

    listagiornalisti

--- news/urls.py
+++ news/urls.py
@@ -1,18 +1,15 @@
-"""from django.urls import path
-from .views import home
-
-app_name= 'news'
-
-urlpatterns=[
-    path('',home, name="homeview"),
-]"""
 from django.urls import path
-from .views import home, articoloDetailView
+from .views import home, ArticoloDetailViewCB, ArticoloListView, GiornalistaDetailViewCB, GiornalistaListView  #articoloDetailView
 
 app_name= 'news'
 
 urlpatterns=[
     path('',home, name="homeview"),
-    path("articoli/<int:pk>", articoloDetailView, name="articolo_detail")
+    #path("articoli/<int:pk>", articoloDetailView, name="articolo_detail")
+    path("articoli/<int:pk>", ArticoloDetailViewCB.as_view(), name="articolo_detail"),
+    path("lista_articoli/", ArticoloListView.as_view(), name="lista_articoli"),
+    #path("lista_giornalisti/", ArticoloListView.as_view(), name="lista_giornalisti")
+    path("giornalisti/<int:pk>", GiornalistaDetailViewCB.as_view(), name="giornalista_detail"),
+    path("lista_giornalisti/", GiornalistaListView.as_view(), name="lista_giornalisti"),
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,26 +1,7 @@
-"""from django.http import HttpResponse
-from .models import Articolo, Giornalista
-
-# Create your views here.
-def home(request):
-    #a= ""
-    a=[] # lista
-    #g= ""
-    g=[] # lista
-    for art in Articolo.objects.all():
-        #a +=(art.titolo + "<br>")
-        a.append(art.titolo)
-    for gio in Giornalista.objects.all():
-        #g +=(gio.nome + "<br>")
-        g.append(gio.nome) # utilizzo append perchè è una lista 
-    #response= "Articoli:<br>" + a + "<br>Giornalisti:<br>"+ g
-    response= str(a) + "<br>" + str(g) #trasformo le liste in stringhe 
-    print(response)
-
-    return HttpResponse("<h1>"+ response + "</h1>")"""
-
 from django.shortcuts import render, get_object_or_404
 from .models import Articolo, Giornalista
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 def home(request):
     articoli= Articolo.objects.all()
@@ -33,5 +14,35 @@ def articoloDetailView(request, pk):
     articolo = get_object_or_404(Articolo, pk=pk)
     context={"articolo": articolo}
     return render(request, "articolo_detail.html", context)
+
+class ArticoloDetailViewCB(DetailView):
+    model= Articolo
+    template_name= "articolo_detail.html"
+
+class ArticoloListView(ListView):
+    model= Articolo
+    template_name= "lista_articoli.html"
+
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context["articoli"] = Articolo.objects.all()
+        return context
+    
+class GiornalistaDetailViewCB(DetailView):
+    model= Giornalista
+    template_name= "giornalista_detail.html"
+
+class GiornalistaListView(ListView):
+    model= Giornalista
+    template_name= "lista_giornalisti.html"
+
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context["giornalisti"] = Giornalista.objects.all()
+        return context
+
+
+
+
 
 

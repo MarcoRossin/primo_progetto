4d7ddced540c17d75f9f04caa0f83ee870a029a7
MarcoRossin <marco.rossin04@gmail.com>
2023-02-27 08:23:53 +0100

    api

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, ArticoloDetailViewCB, ArticoloListView, GiornalistaDetailViewCB, GiornalistaListView  #articoloDetailView
+from .views import home, ArticoloDetailViewCB, ArticoloListView, GiornalistaDetailViewCB, GiornalistaListView, giornalisti_list_api, articoli_list_api, giornalista_api, articolo_api, tabella_giornalisti, tabella_articoli, #articoloDetailView
 
 app_name= 'news'
 
@@ -11,5 +11,12 @@ urlpatterns=[
     #path("lista_giornalisti/", ArticoloListView.as_view(), name="lista_giornalisti")
     path("giornalisti/<int:pk>", GiornalistaDetailViewCB.as_view(), name="giornalista_detail"),
     path("lista_giornalisti/", GiornalistaListView.as_view(), name="lista_giornalisti"),
+
+    path("giornalisti_list_api/", giornalisti_list_api,name="giornalisti_list_api"),
+    path("articoli_list_api/", articoli_list_api,name="articoli_list_api"),
+    path("giornalista_api/<int:pk>", giornalista_api,name="giornalista_api"),
+    path("articolo_api/<int:pk>", articolo_api,name="articolo_api"),
+    path("tabella_giornalisti/", tabella_giornalisti,name="tabella_giornalisti"),
+    path("tabella_articoli/", tabella_articoli,name="tabella_articoli"),
 
 ]
--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Articolo, Giornalista
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.http import JsonResponse
 
 def home(request):
     articoli= Articolo.objects.all()
@@ -41,8 +42,60 @@ class GiornalistaListView(ListView):
         context["giornalisti"] = Giornalista.objects.all()
         return context
 
+def giornalisti_list_api(request):
+    giornalisti=Giornalista.ojects.all()
+    data={'giornalisti':list(giornalisti.values("pk","nome","cognome"))}
+    response=JsonResponse(data)
+    return response
+
+def giornalista_api(request,ok):
+    try:
+        giornalista=Giornalista.ojects.get(pk=pk)
+        data={'giornalista':{
+            "nome":giornalista.nome,
+            "cognome":giornalista.cognome,
+            }    
+        }
+        response=JsonResponse(data)
+    except Giornalista.DoesNoExist:
+        response=JsonResponse({
+            "error":{
+                "code":404,
+                "message":"Giornalista non trovato"
+            }},
+            status=404)
+    return response
+
+def articoli_list_api(request):
+    articoli=Articolo.ojects.all()
+    data={'articoli':list(articoli.values("titolo","contenuto","giornalista"))}
+    response=JsonResponse(data)
+    return response
+
+def articolo_api(request,ok):
+    try:
+        articolo=Articolo.ojects.get(pk=pk)
+        data={'articolo':{
+            "titolo":articolo.nome,
+            "giornalista":articolo.cognome,
+            }    
+        }
+        response=JsonResponse(data)
+    except Articolo.DoesNoExist:
+        response=JsonResponse({
+            "error":{
+                "code":404,
+                "message":"Articolo non trovato"
+            }},
+            status=404)
+    return response
 
 
+def tabella_giornalisti(request):
+    return render(request, "tabella_giornalisti.html")
+
+def tabella_articoli(request):
+    return render(request, "tabella_articoli.html")
 
 
 
